9163c19102be2975087c0b0fc26f532aaa7d93bb
refinedPath <208536365+RefinedPath@users.noreply.github.com>
2026-04-06 15:36:50 -0400

    W1D5: Add CSS styling and loading/error classes

--- W1D5/post-explorer-project/js/dom.js
+++ W1D5/post-explorer-project/js/dom.js
@@ -5,10 +5,13 @@ export function renderPosts(posts) {
 
   if (posts === null) {
     postsHtml = 'Error loading data.';
+    searchResultsEl.className = 'error-loading center-it';
   } else if(posts.length === 0) {
     postsHtml = 'No posts found.';
+    searchResultsEl.className = 'center-it';
   } else {
     postsHtml = posts.map(({ title, body }) => `<li>${title}<br>${body}</li>`).join('');
+    searchResultsEl.className = '';
   }
   
   searchResultsEl.innerHTML = postsHtml;
