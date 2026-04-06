acc3243e101f03caff5335833d595b6379dd0d9f
refinedPath <208536365+RefinedPath@users.noreply.github.com>
2026-04-06 14:24:32 -0400

    W1D5: Add loading, error, and empty states

--- W1D5/post-explorer-project/js/dom.js
+++ W1D5/post-explorer-project/js/dom.js
@@ -1,7 +1,15 @@
 export function renderPosts(posts) {
   const searchResultsEl = document.getElementById('searchResults');
 
-  const postsHtml = posts.map(({ title, body }) => `<li>${title}<br>${body}</li>`).join('');
+  let postsHtml;
+
+  if (posts === null) {
+    postsHtml = 'Error loading data.';
+  } else if(posts.length === 0) {
+    postsHtml = 'No posts found.';
+  } else {
+    postsHtml = posts.map(({ title, body }) => `<li>${title}<br>${body}</li>`).join('');
+  }
   
   searchResultsEl.innerHTML = postsHtml;
 }
